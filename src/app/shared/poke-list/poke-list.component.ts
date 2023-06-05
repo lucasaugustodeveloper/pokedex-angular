d021e5bcd1f10905d1f45ee117b288fb52ae8667
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-06-05 10:23:08 -0300

    add integration of api

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
@@ -6,14 +6,15 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  public getAllPokemons: any;
+
   constructor(
     private pokeApiService: PokeApiService
   ) {}
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
-      res => res
-    );
+      ({ results }) => this.getAllPokemons = results);
   }
 }
