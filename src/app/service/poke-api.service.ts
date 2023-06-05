0899ffdf74c1ea53e91ccba0fb6fd878e06793ee
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-06-05 09:27:24 -0300

    update fetch poke api and create new function for get pokemons

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +14,18 @@ export class PokeApiService {
     return this.http.get<any>(this.url)
       .pipe(
         tap(res => res),
-        tap(res => {
-          console.log('res => ', res);
+        tap(({ results }) => {
+          results.map((pokemon: any) => {
+            this.apiGetPokemons(pokemon.url)
+              .subscribe(res => pokemon.status = res);
+          })
         })
       );
+  }
+
+  public apiGetPokemons(url: string): Observable<void> {
+    return this.http.get<any>(url).pipe(
+      map(res => res)
+    );
   }
 }
